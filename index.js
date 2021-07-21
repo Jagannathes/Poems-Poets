fefdadbb651c2bfec720dddebe2629a9dfab21ab
Jagannathes <75128235+Jagannathes@users.noreply.github.com>
2021-07-21 12:38:40 +0530

    made a rudimentary navbar and added script for fetching 20 random poems and displaying it on homepage

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 //const fetch = require("node-fetch");
-async function forced(){
-    const _news = await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=5b7484d18ce84ca8ae8cec6ebe0ffc85")
-    const news = await _news.json()
-    console.log(news)
+
+// this function will be called once the homepage is loaded
+async function onLoad(){
+    //fetching information about 20 random poems from poetrydb API and convertig to json 
+    const _poem = await fetch("https://poetrydb.org/random/20")
+    const poem = await _poem.json()
+    console.log(poem)
+    //variable for storing the content to be displayed in HTML
     var newsCard = ""
-    for(let i=0, n = news.articles; i < news.articles.length;i++)
+    for(let i=0, n = poem; i < poem.length;i++)
 	{
 
-     newsCard = newsCard+"<div><a href=${n[i].url}><img src="+n[i].urlToImage+"><br><h4>"+n[i].title+"</h4>"+n[i].description+"</a> </div>"
+     newsCard = newsCard+"<div><h4>"+n[i].title+"</h4><h5>"+n[i].author+"</h5>";
+     //  loop for iterating through the first 4 lines of the poem and adding them to newsCard variable 
+
+     for(let j=0, m=n[i].lines; j<Math.min(4,n[i].lines.length);j++){
+        newsCard = newsCard+m[j]+"<br>";
+     }
+
+
+     newsCard+="</div>"
                      
     }
+    // selecting  HTML div based on its ID and changing its content to that of newsCard variable 
     tophead=document.getElementById("tophead")
     tophead.innerHTML=newsCard;
 }
-forced(); 
+
+onLoad(); 
