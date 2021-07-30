60d992e96b9eaedf463a04f0ede8dfbbf48f4f41
Jagannathes <75128235+Jagannathes@users.noreply.github.com>
2021-07-30 18:49:54 +0530

    changed the theme and interface

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-## Poems and Poets 
+# Poems and Poets 
+A simple static website to find poems by poet and title, using poetryDB API. 
+
+## Built with
+HTML
+CSS
+Javascript
 
 
--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 //const fetch = require("node-fetch");
-
+var newsCard = "<h2>Randam Picks</h2><div class =\'random\'>"
 // this function will be called once the homepage is loaded
 async function onLoad(){
     //fetching information about 20 random poems from poetrydb API and convertig to json 
-    const _poem = await fetch("https://poetrydb.org/random/20")
-    const poem = await _poem.json()
+    let _poem = await fetch("https://poetrydb.org/random/20")
+    let poem = await _poem.json()
     console.log(poem)
     //variable for storing the content to be displayed in HTML
-    var newsCard = ""
+    
     for(let i=0, n = poem; i < poem.length;i++)
 	{
 
-     newsCard = newsCard+"<div onclick=\"window.location=\'result.html?"+n[i].title+"\';\"><h4>"+n[i].title+"</h4><h5>"+n[i].author+"</h5>";
+     newsCard = newsCard+"<a href ='result.html?"+n[i].title+"\'><div><h4>"+n[i].title+"</h4><h5>BY "+n[i].author.toUpperCase()+"</h5>";
      //  loop for iterating through the first 4 lines of the poem and adding them to newsCard variable 
 
-     for(let j=0, m=n[i].lines; j<Math.min(4,n[i].lines.length);j++){
+     for(let j=0, m=n[i].lines; j<Math.min(2,n[i].lines.length);j++){
         newsCard = newsCard+m[j]+"<br>";
      }
 
 
-     newsCard+="</div>"
+     newsCard+="</div></a>"
                      
     }
+    newsCard+="</div>"
     // selecting  HTML div based on its ID and changing its content to that of newsCard variable 
     tophead=document.getElementById("tophead")
     tophead.innerHTML=newsCard;
@@ -30,3 +31,38 @@ async function onLoad(){
 }
 
 onLoad(); 
+var searchPoems = document.getElementById("search");
+const drawListPoems = async () => {
+    if(searchPoems.value!= ""){
+        let resultDiv=document.getElementById("tophead")
+        resultDiv="<div class = \"loadresult\"><div>"+newsCard;
+        let _poem = await fetch('https://poetrydb.org/title/'+searchPoems.value)
+        let poem = await _poem.json()
+        console.log(poem);
+        //variable for storing the content to be displayed in HTML
+        let result = "<div class =\'result\'>"
+        for(let i=0, n = poem; i < Math.min(6,poem.length);i++)
+        {
+    
+         result =  result+"<div><h4><a href ='result.html?"+n[i].title+"\'>"+n[i].title+"</a></h4><h5>BY "+n[i].author.toUpperCase()+"</h5>";
+         //  loop for iterating through the first 4 lines of the poem and adding them to newsCard variable 
+    
+         for(let j=0, m=n[i].lines; j<Math.min(2,n[i].lines.length);j++){
+            result = result+m[j]+"<br>";
+         }
+    
+    
+         result+="</div>"
+        }  
+        result+="</div>"
+        resultDiv=document.getElementById("tophead")
+        resultDiv.innerHTML = "<h2>Results</h2>"+result+newsCard;               
+    }
+}
+
+const debounce = (fn, time, to = 0) => {
+    to ? clearTimeout(to) : (to = setTimeout(drawListPoems, time));
+  };
+
+  
+searchPoems.addEventListener("input", () => debounce(drawListPoems, 1000));
